d7ab4617013fcdb9a6bbd93ea809bdebb13d0e4e
GaelCrud <153111897+GaelCrud@users.noreply.github.com>
2023-12-06 10:45:36 -0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="container-fluid bg-dark fixed-bottom">
+        <div class="row">
+            <div class="col-md text-light text-center py-3">
+                Desarrollado por Gael
+            </div>
+        </div>
+    </footer>
+      
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/[email protected]/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/[email protected]/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
